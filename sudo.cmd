e9422eea293b51c29d3a939b45aab3024041b0c8
Ridhwan-riva <fwindows345@gmail.com>
2021-03-09 10:06:38 +0700

    Initial commit

---
+++ sudo.cmd
@@ -0,0 +1,5 @@
+@echo off
+@echo Set objShell = CreateObject("Shell.Application") > %userprofile%\sudo.tmp.vbs
+@echo args = Right("%*", (Len("%*") - Len("%1"))) >> %userprofile%\sudo.tmp.vbs
+@echo objShell.ShellExecute "%1", args, "", "runas" >> %userprofile%\sudo.tmp.vbs
+@cscript %userprofile%\sudo.tmp.vbs
